a66caae64c5b08ad92a4445bb495f86199540c63
Chen zhichao <chenzhichao33@h-partners.com>
2026-05-15 17:41:37 +0800

    fixbug

--- tools/template.py
+++ tools/template.py
@@ -44,7 +44,7 @@ def get_python_download_url(version):
         exit(1)
         
     py_installer_package = "Python-" + py_latest_version
-    py_installer_url = os.path.join("https://repo.huaweicloud.com/python/", py_latest_version, py_installer_package + ".tgz")
+    py_installer_url = f"https://repo.huaweicloud.com/python/{py_latest_version}/{py_installer_package}.tgz"
     return py_installer_package, py_installer_url, py_latest_version
 
 def get_cann_download_url(cann_chip, version):
